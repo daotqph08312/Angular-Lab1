d36128d65600579a79e842c2c9ae4c6aab3b0c3a
daotqph08312 <52927792+daotqph08312@users.noreply.github.com>
2020-03-20 09:11:27 +0700

    update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,11 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
+import { HeroDetailComponent } from './app/hero-detail/hero-detail.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
+  declarations: [ AppComponent, HelloComponent, HeroDetailComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { 
